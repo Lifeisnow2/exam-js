b6ef04de0afab7d3e826280f6e4d758c47741b38
Julia <nikazemlianika@gmail.com>
2023-05-02 21:34:54 +0300

    Task 10.

---
+++ task 10./math.js
@@ -0,0 +1,15 @@
+export function multiplication(a, b) {
+  return a * b;
+}
+
+export function substraction(a, b) {
+  return a - b;
+}
+
+export function composition(a, b) {
+  return a + b;
+}
+
+export function division(a, b) {
+  return a / b;
+}
